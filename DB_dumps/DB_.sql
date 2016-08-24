d993d765e05893e3b787082545f23a757bc5c271
r-mach <remimach.pro@gmail.com>
2016-08-24 10:39:21 +0200

    Commit DB_dumps avec les textzones actuelles remplies.

---
+++ DB_dumps/DB_.sql
@@ -0,0 +1,81 @@
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('home', 'header', 'fr', 'La solution digitale au profit de la
+performance', '2016-08-16 12:58:14', '2016-08-18 13:04:38');
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('home', 'header', 'en', 'Test headddder EN', '2016-08-16 16:47:59', '2016-08-17 11:18:07');
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('home', 'subhead', 'fr', 'ZetaGamma, agence digitale disposant d’une vaste gamme de services, conçoit des solutions digitales concrètes adaptées aux attentes de sa clientèle.', '2016-08-17 13:35:52', '2016-08-17 13:54:08');
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('home', 'subhead', 'en', 'Zetagamma, digital agency with a wide range of services, designs concrete digital solutions, adapted to cutomer exceptations.
+', '2016-08-17 13:37:18', '2016-08-18 15:06:36');
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('home', 'presta_item1_content', 'fr', 'Nous développons des applications web et mobiles grâces aux dernières technologies. Nous assurons la maintenance de l''application et proposons des formations à nos clients adaptées à leurs besoins d’autonomie.', '2016-08-17 14:08:10', '2016-08-17 14:09:09');
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('home', 'presta_item1_header', 'fr', 'Développement et maintenance', '2016-08-17 14:20:17', '2016-08-17 14:20:17');
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('home', 'presta_item2_content', 'fr', 'Dans le but d''atteindre les objectifs fixés par le client, nous assurons la coordination et la maîtrise des activités durant le déroulement du projet pour un résultat respectant l’équilibre qualité-coût-délai.', '2016-08-17 14:21:37', '2016-08-18 08:21:41');
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('home', 'presta_item2_header', 'fr', 'Gestion de projet
+', '2016-08-17 14:22:24', '2016-08-17 14:22:24');
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('home', 'presta_item3_content', 'fr', 'Nous prenons part à la détermination, l’organisation et la structuration de l’ensemble des choix d’objectifs et de ressources à court, moyen ou long terme en vue d’optimiser le service proposé par notre clientèle et d’accroître sa notoriété.
+', '2016-08-17 14:22:36', '2016-08-17 14:22:36');
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('home', 'presta_item3_header', 'fr', 'Stratégie digitale et marketing
+', '2016-08-17 14:22:36', '2016-08-17 14:22:36');
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('home', 'presta_item4_content', 'fr', 'Nous exerçons notre savoir-faire pour révéler les valeurs et l’identité de nos clients à travers un design unique répondant aux normes techniques et esthétiques.
+', '2016-08-17 14:22:47', '2016-08-17 14:22:47');
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('home', 'presta_item4_header', 'fr', 'Conception graphique
+', '2016-08-17 14:22:47', '2016-08-17 14:22:47');
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('home', 'scrolldown', 'fr', 'Descendre', '2016-08-17 14:24:13', '2016-08-17 14:24:13');
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('home', 'presta_props', 'fr', 'Nos prestations
+', '2016-08-17 14:27:38', '2016-08-22 15:29:29');
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('footer', 'footer_textleft', 'fr', 'Tél : +34 911 376 934 / +33 6 29 52 16 68 contact@zetagamma.fr', '2016-08-17 14:35:47', '2016-08-18 08:16:20');
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('footer', 'footer_textleft', 'en', 'Phone : +34 911 376 934 / +33 6 29 52 16 68
+contact@zetagamma.fr
+', '2016-08-18 08:13:41', '2016-08-18 08:16:10');
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('footer', 'footer_copyright', 'fr', '© 2012 - 2016 - Tous droits réservés ZetaGamma.
+', '2016-08-18 08:17:26', '2016-08-18 08:17:26');
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('footer', 'footer_textmiddle', 'fr', 'Du texte pour remplir ici en attendant que quelque chose de mieux arrive !
+   Voila voila...
+', '2016-08-18 08:17:45', '2016-08-18 08:29:28');
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('menubar', 'mb_item1', 'fr', 'Etude de cas
+', '2016-08-18 10:45:20', '2016-08-22 10:40:54');
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('menubar', 'mb_item1', 'en', 'Expertising
+', '2016-08-18 10:45:33', '2016-08-18 10:46:08');
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('home', 'presta_item1_header', 'en', 'Project management', '2016-08-18 15:05:38', '2016-08-18 15:05:38');
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('home', 'presta_item1_content', 'en', 'In order to achieve the objectives set by the customer, we coordinate and contrôle the activities during the lifetime of the project for a result with regard to quality, time, and costs. 
+
+
+', '2016-08-18 15:05:38', '2016-08-18 15:05:38');
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('home', 'presta_props', 'en', 'Services', '2016-08-18 15:05:40', '2016-08-19 10:28:32');
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('home', 'presta_item3_header', 'en', 'Digital strategy and marketing
+', '2016-08-18 15:08:09', '2016-08-19 11:07:20');
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('home', 'header', 'es', 'Un sitio increible!', '2016-08-18 15:23:11', '2016-08-18 15:23:11');
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('home', 'presta_item3_content', 'en', 'In order to optimise the service provided par by our customer and to  increase awareness, we contribute to the determination, the organisation and the framing of the target and the ressources in the short, medium or long term.
+
+
+', '2016-08-19 10:25:50', '2016-08-19 10:26:49');
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('home', 'presta_item2_header', 'en', 'Development and maintenance', '2016-08-19 10:27:30', '2016-08-19 10:27:30');
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('home', 'presta_item4_header', 'en', 'Graphic design', '2016-08-19 10:27:59', '2016-08-19 10:27:59');
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('home', 'presta_item2_content', 'en', 'We develop web and mobile applications thanks to the verry latest. We ensure the maintenance and provide training for our customer, adapted to their needs for autonomy.
+
+
+', '2016-08-19 10:56:33', '2016-08-19 10:56:33');
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('home', 'presta_item4_content', 'en', 'Through our expertise, we reveal the identity and the values of our custmer de nos clients thanks to an unique design unique which meets technical and esthetic standards.
+
+
+', '2016-08-19 11:06:51', '2016-08-19 11:06:51');
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('home', 'button_3', 'fr', ' En savoir plus', '2016-08-19 11:29:51', '2016-08-22 13:49:55');
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('home', 'etude_cas', 'fr', '
+', '2016-08-19 12:16:01', '2016-08-22 15:29:41');
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('home', 'projects', 'fr', 'Notre dernière étude de cas', '2016-08-19 12:18:20', '2016-08-22 15:29:49');
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('home', 'projet_1_text', 'fr', 'Nous développons des applications web et mobiles grâces aux dernières technologies. Nous assurons la maintenance de l''application et proposons des formations à nos clients adaptées à leurs besoins d’autonomie.
+', '2016-08-19 12:52:03', '2016-08-19 12:52:09');
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('home', 'contact_us_2', 'fr', 'Nous répondrons dès que possible', '2016-08-22 08:47:09', '2016-08-22 08:47:09');
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('home', 'contact_us', 'fr', ' 
+', '2016-08-22 08:47:11', '2016-08-23 15:20:39');
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('menubar', 'mb_item2', 'fr', 'Expertise
+', '2016-08-22 10:40:59', '2016-08-22 10:40:59');
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('menubar', 'mb_item3', 'fr', 'A propos
+', '2016-08-22 10:41:02', '2016-08-22 10:58:23');
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('menubar', 'mb_item0', 'fr', 'Accueil
+', '2016-08-22 10:58:07', '2016-08-22 10:58:07');
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('menubar', 'mb_item4', 'fr', 'Contact
+', '2016-08-22 10:58:26', '2016-08-22 10:58:26');
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('expert', 'header', 'fr', 'Expertise', '2016-08-23 09:05:35', '2016-08-23 09:05:35');
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('expert', 'quotation', 'fr', '“ Vivre une expérience enrichissante au cœur de la maîtrise digitale. ”
+
+François-Xavier GORIN
+', '2016-08-23 09:57:28', '2016-08-23 10:01:31');
+INSERT INTO zetagamma.textzones (id_page, id_textzone, lang, content, created_at, updated_at) VALUES ('study', 'header', 'fr', 'Etude de cas', '2016-08-23 14:10:27', '2016-08-23 14:10:27');
